831932a903692fa6be453faf52eb110ba8534083
Inna Mon <inna.mon@gmail.com>
2019-10-07 08:53:57 -0500

    added a category component with styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Category from './components/Category/Category';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Let's start this jeopary app!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monjo FamBam Jeopardy!!!</h1>
+      <div className="row">
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+      </div>
     </div>
   );
 }
---
+++ src/components/Category/Category.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Category.css';
+
+const Category = () => {
+    return (
+        <div className="category">
+            <p>Category</p>
+        </div>
+    )
+}
+
+export default Category;
