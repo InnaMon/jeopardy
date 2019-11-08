c2b28009b08ef2240150f427aa001224bceefece
Inna Mon <inna.mon@gmail.com>
2019-11-08 15:48:30 -0600

    Updates

---
+++ precache-manifest.5cf2acd77593d2b7c6ac1e3175b833b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613a906b7e646c15d01363b5545f56d8",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "16d07316228334954a3b",
+    "url": "/jeopardy/static/css/main.dcb0833d.chunk.css"
+  },
+  {
+    "revision": "932145a006523c4ce817",
+    "url": "/jeopardy/static/js/2.e9482750.chunk.js"
+  },
+  {
+    "revision": "16d07316228334954a3b",
+    "url": "/jeopardy/static/js/main.935961e1.chunk.js"
+  },
+  {
+    "revision": "5543fe1ddd9bf3dc332c",
+    "url": "/jeopardy/static/js/runtime-main.f5a61539.js"
+  },
+  {
+    "revision": "6473044cd8766c1007d9eab7456c53b4",
+    "url": "/jeopardy/static/media/AmaticSC-Bold.6473044c.woff"
+  },
+  {
+    "revision": "f9f389bbd1915bae3c46375264429946",
+    "url": "/jeopardy/static/media/AmaticSC-Regular.f9f389bb.woff"
+  },
+  {
+    "revision": "cd6c05959f60a7586420e0d5e7c3f4da",
+    "url": "/jeopardy/static/media/Toony.cd6c0595.woff"
+  }
+]);
